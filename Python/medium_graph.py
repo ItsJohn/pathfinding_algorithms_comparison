1f012a8553933668f61ba4fd2e8add92cdc4d648
John Kelly <john.j.kelly94@gmail.com>
2017-02-03 12:35:05 +0000

    python complete

---
+++ Python/medium_graph.py
@@ -0,0 +1,2 @@
+from utils import create_graph
+create_graph('medium_graph.json', 1000)
---
+++ Python/utils.py
@@ -0,0 +1,67 @@
+from networkx.readwrite import json_graph
+import matplotlib.pyplot as plt
+import networkx as nx
+import json
+import timeit
+
+
+def sanitize_input(value, max_node):
+    try:
+        if value:
+            value = int(value)
+        else:
+            print("You didn't enter a value")
+            return False
+    except ValueError:
+        print("Please enter an integer")
+        return False
+    if value >= 1 and value <= max_node:
+        return True
+    else:
+        print("Please enter an integer that is within the option range")
+        return False
+
+
+def check_input(choice, max_nodes):
+    while not sanitize_input(choice, max_nodes):
+        choice = input('Enter option: ')
+    return choice
+
+
+def choose_nodes(max_nodes):
+    print("\nChoose a node between 1 and", max_nodes)
+    print("This will be the node the algorithm will be starting from")
+    choiceA = check_input(input('Enter option: '), max_nodes)
+
+    print("\nChoose another node between 1 and", max_nodes)
+    print("This will be the node the algorithm will be finishing on")
+    choiceB = check_input(input('Enter option: '), max_nodes)
+    return int(choiceA), int(choiceB)
+
+
+def open_file(filename):
+    with open(filename) as fh:
+        data = json.load(fh)
+    return data
+
+
+def create_graph(filename, max_nodes):
+    graph = json_graph.node_link_graph(open_file(filename))
+    layout = nx.circular_layout(graph)
+    astar_elapsed_time, dijkstra_elapsed_time = calculate(graph, max_nodes)
+    print("\nA* execution time:", astar_elapsed_time)
+    print("Dijkstra execution time:", dijkstra_elapsed_time)
+
+
+def calculate(graph, max_nodes):
+    source, destination = choose_nodes(max_nodes)
+
+    astar_start_time = timeit.default_timer()
+    nodes = nx.astar_path(graph, source, destination)
+    astar_elapsed_time = timeit.default_timer() - astar_start_time
+
+    dijkstra_start_time = timeit.default_timer()
+    nodes = nx.dijkstra_path(graph, source, destination)
+    dijkstra_elapsed_time = timeit.default_timer() - dijkstra_start_time
+
+    return astar_elapsed_time, dijkstra_elapsed_time
