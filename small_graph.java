e624a2d08dd9b27ae56cf0f1d7c41ba8c291949e
GsProjects <gearoidlacey@gmail.com>
2017-03-01 16:19:05 +0000

    Fixed Looping Issue

---
+++ small_graph.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+public class small_graph
+{
+		public static void main(String args[]) throws IOException
+		{
+			String filename = "small_graph.csv";
+			dijkstras_alg_java temp = new dijkstras_alg_java(filename);
+			System.out.println("Done");
+		}
+	
+}
