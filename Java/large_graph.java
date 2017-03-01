68569e32c8249f0a1a6c7ca5ab700ce9425f2950
GsProjects <gearoidlacey@gmail.com>
2017-03-01 16:28:19 +0000

    Java Completed

--- Java/large_graph.java
+++ Java/large_graph.java
@@ -4,8 +4,8 @@ public class large_graph
 {
 	public static void main(String args[]) throws IOException
 	{
-	String filename = "large_graph.csv";
-	dijkstras_alg_java temp = new dijkstras_alg_java(filename);
-	temp.start(filename);
+		String filename = "large_graph.csv";
+		utils temp = new utils(filename);
+		System.out.println("Done");
 	}
 }
--- Java/small_graph.java
+++ Java/small_graph.java
@@ -3,9 +3,9 @@ public class small_graph
 {
 		public static void main(String args[]) throws IOException
 		{
-		String filename = "small_graph.csv";
-		dijkstras_alg_java temp = new dijkstras_alg_java(filename);
-		temp.start(filename); 
+			String filename = "small_graph.csv";
+			utils temp = new utils(filename);
+			System.out.println("Done");
 		}
 	
 }
