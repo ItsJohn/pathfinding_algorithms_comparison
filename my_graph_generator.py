61be52edeaa2e5454f87ecb63b06d15617afa24e
GsProjects <gearoidlacey@gmail.com>
2017-03-02 15:16:19 +0000

    Graph generator fully implemented

--- my_graph_generator.py
+++ my_graph_generator.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def createGraph(x):
 	shuffle_list =[]
 	G = nx.Graph()
-	for i in range(1,x):
+	for i in range(1,x+1):
 		G.add_node(i)
 
 	if file_size == 100:
@@ -18,38 +18,39 @@ def createGraph(x):
 		name = 'large_graph.txt'
 
 
-	ii = 1
 	node_list = G.nodes()
+	with open(name, 'a') as outfile:
+			weight = random.uniform(0.0, 1.0)
+			G.add_edge(node_list[0],node_list[len(node_list) -1],weight= weight)
+			outfile.write(str(node_list[0]) + ',' + str(node_list[len(node_list) -1]) + ',' + str(weight) +'\n')
+
+	for i,node in enumerate(node_list):
+		if i is not 0 :
+			with open(name, 'a') as outfile:
+				print(node)
+				node2 = node_list[i-1]
+				print (node2)
+				
+				weight = random.uniform(0.0, 1.0)
+				G.add_edge(node,node2,weight= weight)
+				outfile.write(str(node) + ',' + str(node2) + ',' + str(weight) +'\n')
+				
+	shuffle_list =[]
 	for n in node_list:
 		shuffle_list.append(n)
 	random.shuffle(shuffle_list)
 
-	for i,node in enumerate(reversed(node_list)):
-		if i != 0:
-			with open(name, 'a') as outfile:
-				node2 = node-1
-				weight = random.uniform(0.0, 1.0)
-				G.add_edge(node,node2,weight= weight)
-				outfile.write(str(node) + ',' + str(node2) + ',' + str(weight) +'\n')
-		else:
-			G = G
-
 	index = 0
 	while index <= len(node_list) /2:
-		with open(name, 'a') as outfile:
-			node = node_list[index]
-			node2 = shuffle_list[index]
-			weight = random.uniform(0.0, 1.0)
-			G.add_edge(node,node2,weight= weight)
-			outfile.write(str(node) + ',' + str(node2) + ',' + str(weight) +'\n')
-			index = index + 1
-		
-
-
-	nx.draw(G)
-	plt.show()
-
-
+		if index is not 0:
+			with open(name, 'a') as outfile:
+				node1 = node_list[index]
+				node3 = shuffle_list[index]
+				temporary = (node,node3)
+				if temporary not in G.edges() and node1 is not node3:
+					G.add_edge(node1,node3,weight= weight)
+					outfile.write(str(node1) + ',' + str(node3) + ',' + str(weight) +'\n')
+		index = index +1	
 
 file_size = int(input('Enter the size of the graph (100, 1000 or 10,000): '))
 
